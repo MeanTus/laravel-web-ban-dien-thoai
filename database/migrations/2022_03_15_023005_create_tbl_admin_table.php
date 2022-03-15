007636eef49aa3aac0c819891856beb59eee6b8a
quan <s5octiius@gmail.com>
2022-03-15 10:16:40 +0700

    fix

--- database/migrations/2022_03_15_023005_create_tbl_admin_table.php
+++ database/migrations/2022_03_15_023005_create_tbl_admin_table.php
@@ -14,12 +14,12 @@ class CreateTblAdminTable extends Migration
     public function up()
     {
         Schema::create('tbl_admin', function (Blueprint $table) {
-            $table->id();
-            $table->increments('admin_id');
-            $table->string('admin_email', 100);
+            $table->Increments('admin_id');
+            $table->string('admin_email',100);
             $table->string('admin_password');
             $table->string('admin_name');
             $table->string('admin_phone');
+            
             $table->timestamps();
         });
     }
